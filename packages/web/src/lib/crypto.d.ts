bb918e33207c6204867eeea5a707b5958cdf4053
Kaasbroodjeskanaal <markmerkens6@gmail.com>
2026-06-16 19:34:48 +0200

    git

--- packages/web/src/lib/crypto.d.ts
+++
@@ -1,5 +0,0 @@
-export declare const GC_PREFIX = "gc:";
-export declare function encryptText(plaintext: string): string;
-export declare function decryptText(ciphertext: string): string;
-export declare function maybeDecryptText(text: string, isEncrypted?: boolean): string;
-//# sourceMappingURL=crypto.d.ts.map
