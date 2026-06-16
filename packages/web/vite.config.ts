c3ceb1e7aeea88635441870e952f0b2b42afbbdb
Kaasbroodjeskanaal <markmerkens6@gmail.com>
2026-06-16 19:21:19 +0200

    bruh

--- packages/web/vite.config.ts
+++ packages/web/vite.config.ts
@@ -12,6 +12,7 @@ export default defineConfig({
       '@ftjm/shared': path.resolve(__dirname, '../../packages/shared/src'),
     },
   },
+  base: './',
   build: {
     outDir: 'dist',
     emptyOutDir: true,
